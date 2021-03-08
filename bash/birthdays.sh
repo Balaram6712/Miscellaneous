1137249cb94c61593a53a7d226060dafe055bd40
Sai Balaram Korrapati <es18btech11011@iith.ac.in>
2021-03-09 02:36:32 +0530

    Added like a template

--- bash/birthdays.sh
+++ bash/birthdays.sh
@@ -5,35 +5,5 @@ MONTH_NOW=$(date +"%m")
 
 if [[ $DATE_TODAY -eq 20 ]] && [[ $MONTH_NOW -eq 10 ]]
 then
-	echo -e "\n" | mailx -s "Happy birthday ra Gv!" -A images.jpeg cs18btech11014@iith.ac.in 
-fi				#gv bday
-
-if [[ $DATE_TODAY -eq "02" ]] && [[ $MONTH_NOW -eq 05 ]]
-then	
-	echo -e "\n" | mailx -s "Happy birthday ra shit!" -A images.jpeg cs18btech11018@iith.ac.in
-fi				#nishith bday
-
-if [[ $DATE_TODAY -eq 30 ]] && [[ $MONTH_NOW -eq 06 ]]
-then
-	echo -e "\n" | mailx -s "Happy birthday ra Srikar!" -A images.jpeg cs18btech11034@iith.ac.in
-fi				#srikar bday
-
-if [[ $DATE_TODAY -eq 04 ]] && [[ $MONTH_NOW -eq 05 ]]
-then	
-	echo -e "\n" | mailx -s "Happy birthday ra puppy bowl!" -A ee18btech11049@iith.ac.in 
-fi				#laxman bday
-
-if [[ $DATE_TODAY -eq 05 ]] && [[ $MONTH_NOW -eq 09 ]]
-then
-	echo -e "\n" | mailx -s "Happy birthday pro!" -A images.jpeg es18btech11013@iith.ac.in 
-fi				#nishant bday 
-
-if [[ $DATE_TODAY -eq 17 ]] && [[ $MONTH_NOW -eq 02 ]]
-then
-	echo -e "\n" | mailx -s "Happy birthday ra!" -A images.jpeg cs18btech11037@iith.ac.in
-fi	
-			#asish bday
-if [[ $DATE_TODAY -eq 07 ]] && [[ $MONTH_NOW -eq 01 ]]
-then
-	echo -e "\n" | mailx -s "Happy birthday Vish" -A images.jpeg cs18btech11013@iith.ac.in 
-fi			#vish bday		
+	echo -e "\n" | mailx -s "Happy birthday!" -A images.jpeg <mail-id>
+fi				#bday	also attaching a image 
