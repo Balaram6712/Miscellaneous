0b985c630ed3a7c08c4c7e7fe187af0da31d6ba9
Sai Balaram Korrapati <es18btech11011@iith.ac.in>
2021-03-09 02:38:09 +0530

    Read me also update

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Bash scripts for automation.
 
 - Set up a cron job to send data usage by my laptop
 - Another can be used to send or spam an inbox of anyone mail by filling corresponding mail-id, simply
-
+-just like a template which can be used, but before that u need to set up the SMTP server automation of e-mail and p[assword in a conf file for email automation by the cron job every day
+ 
 Score parser
 - Python score parse by web-link so that it can displayed in small space using tkinter and updated by running a cron job scheduled
--- score.py
+++ score.py
@@ -4,6 +4,7 @@
 import requests
 import bs4
 from tkinter import *
+import time
 
 class window(Frame):
 	def __init__(self, master=None):
@@ -14,23 +15,25 @@ class window(Frame):
 
 
 
-link = 'https://www.cricbuzz.com/live-cricket-scores/31449/5th-match-csa-t20-challenge-2021	'
-
-res = requests.get(link)
-
-
-bs = bs4.BeautifulSoup(res.text,'html.parser')
-
-score = bs.select('.cb-col-100 .cb-col .cb-col-scores')
-current = bs.select('.cb-key-lst-wrp .cb-font-12')
-
-for i in score:
-	print(i.getText().strip())
+while True:
+	link = 'https://www.cricbuzz.com/live-cricket-scores/32258/ind-vs-eng-3rd-test-england-tour-of-india-2021'
+	res = requests.get(link)
+	bs = bs4.BeautifulSoup(res.text,'html.parser')
+	score = bs.select('.cb-col-100 .cb-col .cb-col-scores')
+	cur = bs.select('.cb-min-itm-rw')
 
 
-root = TK()
+	for i in score:
+		print(i.getText())
+
+	for i in cur:
+		print(i.div.div.getText())
+
+	print('')
+	time.sleep(300)
+"""root = TK()
 app = window(root)
 root.wm_title("Score")
 root.geometry("100x100")
-root.mainloop()
+root.mainloop()"""
 
